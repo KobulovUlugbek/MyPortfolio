f4736b540d6590bdbf5fbff9c5cd8a5ed88b5cbe
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-08-14 07:45:42 +0200

    responsive body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartsiteComponent } from './startsite/startsite.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
   { path:'', component: StartsiteComponent },
-  { path: 'imprint', component: ImprintComponent }
-
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import AOS from "aos";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-
   navigateToSection() {
     const section = document.getElementById('header-section');
     if (section) {
@@ -39,7 +39,7 @@ export class ContactComponent implements OnInit {
 
   target!: HTMLInputElement;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     AOS.init();
@@ -47,9 +47,14 @@ export class ContactComponent implements OnInit {
 
   async sendMail() {
     this.emailSent = true;
+
     setTimeout(() => {
       this.showSpanMsg = true;
       this.addClassToButton = true;
+
+      setTimeout(() => {
+        this.resetForm(); // Call the reset method after 2 seconds
+      }, 2000);
     }, 800);
     let nameField = this.nameField.nativeElement;
     let emailField = this.emailField.nativeElement;
@@ -62,8 +67,48 @@ export class ContactComponent implements OnInit {
 
     await fetch('https://ulugbek-kobulov.com/send_mail/send_mail.php', {
       method: 'POST',
-      body: formData
+      body: formData,
     });
+  }
+  resetForm() {
+    this.nameField.nativeElement.value = '';
+    this.emailField.nativeElement.value = '';
+    this.messageField.nativeElement.value = '';
+
+    // Reset other flags and styles
+    this.showWarningName = false;
+    this.showWarningEmail = false;
+    this.showWarningMessage = false;
+    this.showGreenCheckName = false;
+    this.showGreenCheckEmail = false;
+    this.showGreenCheckMessage = false;
+    this.showLoader = false;
+    this.emailSent = false;
+    this.showSpanMsg = false;
+    this.addClassToButton = false;
+
+    // Clear CSS classes
+    this.nameField.nativeElement.classList.remove(
+      'has-content',
+      'empty-focused',
+      'input-bg-warning'
+    );
+    this.emailField.nativeElement.classList.remove(
+      'has-content',
+      'empty-focused',
+      'input-bg-warning'
+    );
+    this.messageField.nativeElement.classList.remove(
+      'has-content',
+      'empty-focused',
+      'input-bg-warning'
+    );
+
+    // Scroll to the top of the form if needed
+    const formSection = document.getElementById('myForm');
+    if (formSection) {
+      formSection.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   onBlur(event: Event) {
@@ -71,55 +116,53 @@ export class ContactComponent implements OnInit {
     this.checkInputState();
   }
 
-
   onInput(inputType: string) {
     this.checkInputState();
     this.checkInputValue(inputType);
     this.showGreenCheckImage(inputType);
   }
 
-
   checkInputState() {
-    this.target.value.length > 0 ? this.changeInputsGreen() : this.changeInputsRed();
+    this.target.value.length > 0
+      ? this.changeInputsGreen()
+      : this.changeInputsRed();
   }
 
-
   changeInputsGreen() {
-    this.target.classList.add("has-content");
-    this.target.classList.remove("empty-focused");
+    this.target.classList.add('has-content');
+    this.target.classList.remove('empty-focused');
     this.target.classList.remove('input-bg-warning');
   }
 
-
   changeInputsRed() {
-    this.target.classList.remove("has-content");
-    this.target.classList.add("empty-focused");
+    this.target.classList.remove('has-content');
+    this.target.classList.add('empty-focused');
     this.target.classList.add('input-bg-warning');
   }
 
-
   checkInputValue(inputType: string) {
-    this.target.value.length > 0 ? this.showRequiredMessage(inputType) : this.hideRequiredMessage(inputType);
+    this.target.value.length > 0
+      ? this.showRequiredMessage(inputType)
+      : this.hideRequiredMessage(inputType);
   }
 
-
   showRequiredMessage(inputType: string) {
-    let oneInputField = 'showWarning' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    let oneInputField =
+      'showWarning' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
     this[`${oneInputField}`] = false;
   }
 
-
   hideRequiredMessage(inputType: string) {
-    let oneInputField = 'showWarning' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    let oneInputField =
+      'showWarning' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
     this[`${oneInputField}`] = true;
   }
 
-
   showGreenCheckImage(inputType: string) {
-    let oneInputField = 'showGreenCheck' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    let oneInputField =
+      'showGreenCheck' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
     this[`${oneInputField}`] = this.target.value.length > 0;
   }
-
 
   onFocus(event: Event, inputType: string) {
     this.target = event.target as HTMLInputElement;
@@ -130,9 +173,9 @@ export class ContactComponent implements OnInit {
     }
   }
 
-
   showRequiredTextOnFocus(inputType: string) {
-    let oneInputField = 'showWarning' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    let oneInputField =
+      'showWarning' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
     this[`${oneInputField}`] = true;
   }
 }
